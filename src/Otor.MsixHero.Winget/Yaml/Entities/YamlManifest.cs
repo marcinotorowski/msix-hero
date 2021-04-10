cb15f29c79ae1bed6c710303b55a6b7d61606baf
Marcin Otorowski <marcin.otorowski+git@gmail.com>
2021-04-10 22:10:16 +0200

    Add default values for ManifestType and ManifestVersion (#110)

--- src/Otor.MsixHero.Winget/Yaml/Entities/YamlManifest.cs
+++ src/Otor.MsixHero.Winget/Yaml/Entities/YamlManifest.cs
@@ -240,7 +240,7 @@ namespace Otor.MsixHero.Winget.Yaml.Entities
         //   "pattern": "^(0|[1-9][0-9]{0,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])(\\.(0|[1-9][0-9]{0,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])){2}$",
         //   "description": "The manifest syntax version"
         // }
-        [YamlMember(Order = 15, SerializeAs = typeof(string))]
-        public Version ManifestVersion { get; set; }
+        [YamlMember(SerializeAs = typeof(string))]
+        public Version ManifestVersion { get; set; } = new Version(1, 0 ,0);
     }
 }
